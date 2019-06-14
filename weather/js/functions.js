355c36bcd679dee559e5b6f4309f02aac8425824
ezrarojas <roj17001@byui.edu>
2019-06-14 12:35:07 -0600

    update

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -19,3 +19,49 @@ wc = (wc > temp)?temp:wc;
  feelTemp.innerHTML = wc;
  }
  buildWC(speed, temp);
+ const direction = "NNE"; //Set my own value
+ windDial(direction);
+ // Wind Dial Function
+function windDial(direction){
+// Get the wind dial container
+const dial = document.getElementById("dial");
+// Determine the dial class
+switch (direction){
+    case "North":
+    case "N":
+     dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+     break;
+    case "NE":
+    case "NNE":
+    case "ENE":
+     dial.setAttribute("class", "ne");
+     break;
+    case "NW":
+    case "NNW":
+    case "WNW":
+     dial.setAttribute("class", "nw");
+     break;
+    case "South":
+    case "S":
+     dial.setAttribute("class", "s");
+     break;
+    case "SE":
+    case "SSE":
+    case "ESE":
+     dial.setAttribute("class", "se");
+     break;
+    case "SW":
+    case "SSW":
+    case "WSW":
+     dial.setAttribute("class", "sw");
+     break;
+    case "East":
+    case "E":
+     dial.setAttribute("class", "e");
+     break;
+    case "West":
+    case "W":
+     dial.setAttribute("class", "w");
+     break;
+   }
+}
